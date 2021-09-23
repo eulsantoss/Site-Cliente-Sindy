a8005ca0ed5a51f12ccd853146539a747c1a976c
Lucas Santos <85079606+eulsantoss@users.noreply.github.com>
2021-09-22 23:46:53 -0300

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,25 @@
+
+
+function sendOrder1(){
+    window.open('https://wa.me/5521976353377?text=Ol%C3%A1!%20Gostaria%20de%20comprar%20o%20Conjunto%20Lara%20Marsala!', '_blank');
+}
+
+function sendOrder2(){
+    window.open('https://wa.me/5521976353377?text=Ol%C3%A1!%20Gostaria%20de%20comprar%20o%20Conjunto%20Tule%20com%20Aro!', '_blank')
+}
+
+function sendOrder3(){
+    window.open('https://wa.me/5521976353377?text=Ol%C3%A1!%20Gostaria%20de%20comprar%20o%20Conjunto%20Sara%20Vermelho!', '_blank')
+}
+
+function sendOrder4(){
+    window.open('https://wa.me/5521976353377?text=Ol%C3%A1!%20Gostaria%20de%20comprar%20o%20Conjunto%20Lara%20Preto!', '_blank')
+}
+
+function sendOrder5(){
+    window.open('https://wa.me/5521976353377?text=Ol%C3%A1!%20Gostaria%20de%20comprar%20o%20Body%20Duda%20Vermelho!', '_blank')
+}
+
+function sendOrder6(){
+    window.open('https://wa.me/5521976353377?text=Ol%C3%A1!%20Gostaria%20de%20comprar%20o%20Conjunto%20Lara%20Vermelho!', '_blank')
+}
